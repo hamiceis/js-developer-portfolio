59b1d67b0684c9b7e6799202423fcb5e19bc4ef2
hamiceis <hamiceis@hotmail.com>
2023-01-19 13:06:39 -0300

    atualizando softskills

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+function updateDataProfile(profileData){
+  const photo = document.getElementById('profile.photo')
+  photo.src = profileData.photo
+  photo.alt = profileData.name
+
+  const name = document.getElementById('profile.name')
+  name.textContent = profileData.name
+
+  const job = document.getElementById('profile.job')
+  job.textContent = profileData.job
+
+  const location = document.getElementById('profile.location')
+  location.textContent = profileData.location
+
+  const phone = document.getElementById('profile.phone')
+  phone.parentElement.href = `tel:+55${profileData.phone}`
+  phone.innerText = profileData.phone
+
+  const mail = document.getElementById('profile.email')
+  mail.parentElement.href = `mailto:${profileData.email}`
+  mail.innerText = profileData.email
+}
+
+function updateSkills(profileData){
+  const skills = document.getElementById('profile.skills.softskills')
+  const skill = profileData.skills.softSkills?.reduce((acc, skill) => {
+    return acc += `<li>${skill}</li>`
+  },'')
+  skills.innerHTML = skill
+}
+
+(async () => {
+  const profileData = await fetchProfileData()
+  updateDataProfile(profileData)
+  updateSkills(profileData)
+})()
